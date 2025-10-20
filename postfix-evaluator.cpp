5cc19353c00711f1bc011dcd1b5fe66e0d64618e
Enes Abulgayt <2309015698@stu.istinye.edu.tr>
2025-10-20 10:08:09 +0300

    Refactor postfix evaluator stack operations
    Refactor stack implementation and improve error handling.

--- postfix-evaluator.cpp
+++ postfix-evaluator.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
+#include <cstdlib>
 using namespace std;
+int stack[1];
+int top = 0;
+int sizeofstack = sizeof(stack)/sizeof(stack[0]);
 
-int stack[20], top = 0;
-int stacksize =sizeof(stack)/sizeof(stack[0]);
-
-void push(int a){
-    if(top<=stacksize-1){
-        stack[top]=a;
+void push(int num){
+    if(top <= sizeofstack-1){
+        stack[top] = num;
         top++;
-    }
-    else{
-        cout<<"stack is overflow";
+    }else{
+        cout << "Error: Stack overflow (cannot push more elements).";
+        exit(1);
     }
 }
+
 int pop(){
-    if(top == 0){
-        cout<<"stack is undderflow";
-        return 0;
+    if(top <= 0){
+        cout<<" Error: Stack underflow (no elements to pop).";
+        exit(1);
     }
     else{
         top--;
@@ -25,28 +27,36 @@ int pop(){
 }
 int eval(char ch, int pop1, int pop2){
     switch(ch){
-        case '+':return(pop1+pop2);
-        case '-':return(pop1-pop2);
-        case '/':return(pop1/pop2);
-        case '*':return(pop1*pop2);
+        case '+':return(pop1 + pop2);
+        case '-':return(pop1 - pop2);
+        case '*':return(pop1 * pop2);
+        case '/':
+        if (pop2 == 0) {
+                cout <<"Error: Division by zero." << endl;
+                exit(1);
+            }else{
+            return (pop1 / pop2);}
+        default:
+            cout << "Invalid operator: " << ch << endl;
+            cout<<"your exprition is wrong";
+            exit(1);
     }
 }
 int main(){
     char exp[100], ch;
-    cout<<"enter the Postfix exprtion :"<< endl;
+    int num, pop1, pop2;
+    cout << "please enter the postfix exprition"<<endl;
     cin>>exp;
-    int pop1, pop2;
-    for(int i = 0;exp[i]!='\0';i++){
-        ch=exp[i];
-        if(ch>='0' && ch<='9'){
-            push(ch-'0');
-        }
-        else{
+    for(int i = 0; exp[i]!='\0'; i++){
+        ch = exp[i];
+        if(ch >= '0' && ch <= '9'){
+            num = ch -'0';
+            push(num);
+        }else{
             pop2 = pop();
             pop1 = pop();
             push(eval(ch, pop1, pop2));
         }
     }
     cout <<"evaluwated number is : " << pop();
-    
 }
